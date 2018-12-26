de2a8b0d38c20b4b590147c2ddb628d587e1e689
Oleh Yashchyshy <oyashchyshyn@bizico.com>
2018-12-26 14:56:04 +0200

    add_two_algorithms

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Enumerable.Range(1, 100).ToArray();
+            var number = -1;
+
+            BinarySearch(number, array);
+
+            Console.ReadLine();
+        }
+
+        static void BinarySearch(int number, int[] arrayOfNumbers)
+        {
+            Array.Sort(arrayOfNumbers);
+
+            int low = 0;
+            int high = arrayOfNumbers.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int guess = arrayOfNumbers[mid];
+
+                if (number == guess)
+                {
+                    Console.WriteLine($"Position: {mid}");
+                    return;
+                }
+
+                if (number < guess)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            Console.WriteLine("Not found");
+        }
+    }
+}
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 3, 25, 2, 12, 7, 130, 0, 110 };
+
+            foreach (var number in SelectionSort(numbers))
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+
+        static int[] SelectionSort(int[] array)
+        {
+            var sortedArray = new int[array.Length];
+            var listOfItems = array.ToList();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                var currentSmallest = FindSmallest(listOfItems);
+                sortedArray[array.Length - 1 - i] = currentSmallest;
+                listOfItems.Remove(currentSmallest);
+            }
+
+            return sortedArray;
+        }
+
+        static int FindSmallest(List<int> list)
+        {
+            int smallest = list[0];
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (smallest > list[i])
+                {
+                    smallest = list[i];
+                }
+            }
+
+            return smallest;
+        }
+    }
+}
