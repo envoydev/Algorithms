66a0ac96b662ab06f93abf0717bfd65980d4d886
Oleh Yashchyshy <oyashchyshyn@bizico.com>
2018-12-29 13:03:50 +0200

    add BFS algorithm

---
+++ BreadthFirstSearch/BreadthFirstSearch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BreadthFirstSearch
+{
+    public static class BreadthFirstSearch
+    {
+        public static string Search(string name, IDictionary<string, string[]> connections)
+        {
+            var queueSearch = new Queue<string>(connections[name]);
+            var searched = new List<string>();
+
+            while (queueSearch.Any())
+            {
+                var person = queueSearch.Dequeue();
+
+                if (!searched.Contains(person))
+                {
+                    if (NeededPerson(person)) return person;
+
+                    queueSearch = new Queue<string>(queueSearch.Concat(connections[person]));
+                    searched.Add(person);
+                }
+            }
+
+            return "Not Found";
+        }
+
+        static bool NeededPerson(string name)
+        {
+            if (name.Contains("m")) return true;
+
+            return false;
+        }
+    }
+}
---
+++ BreadthFirstSearch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BreadthFirstSearch
+{
+    class Program
+    {
+        readonly static Dictionary<string, string[]> _graph = new Dictionary<string, string[]>();
+
+        static void Main(string[] args)
+        {
+            _graph.Add("you", new[] { "alice", "bob", "claire" });
+            _graph.Add("bob", new[] { "anuj", "peggy" });
+            _graph.Add("alice", new[] { "peggy" });
+            _graph.Add("claire", new[] { "thom", "jonny" });
+            _graph.Add("anuj", Array.Empty<string>());
+            _graph.Add("peggy", Array.Empty<string>());
+            _graph.Add("thom", Array.Empty<string>());
+            _graph.Add("jonny", Array.Empty<string>());
+
+            var result = BreadthFirstSearch.Search("you", _graph);
+
+            Console.WriteLine($"{result} is a seller");
+
+            Console.ReadLine();
+        }
+    }
+}
