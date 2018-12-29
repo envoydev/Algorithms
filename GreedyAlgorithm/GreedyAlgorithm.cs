4ed4b74c627c0f73f5b4caecb0ecd94d10f15bf0
Oleh Yashchyshy <oyashchyshyn@bizico.com>
2018-12-29 17:52:54 +0200

    add Greedly algorithm

---
+++ GreedyAlgorithm/GreedyAlgorithm.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GreedyAlgorithm
+{
+    public static class GreedyAlgorithm
+    {
+        public static HashSet<string> Search(IDictionary<string, HashSet<string>> stations, HashSet<string> statesNeeded)
+        {
+            var finalStations = new HashSet<string>();
+
+            while (statesNeeded.Any())
+            {
+                string bestStation = string.Empty;
+                var coveredStates = new HashSet<string>();
+
+                foreach (var station in stations)
+                {
+                    var covered = new HashSet<string>(statesNeeded.Intersect(station.Value));
+
+                    if (covered.Count > coveredStates.Count)
+                    {
+                        bestStation = station.Key;
+                        coveredStates = covered;
+                    }
+                }
+
+                statesNeeded.RemoveWhere(x => coveredStates.Contains(x));
+                finalStations.Add(bestStation);
+            }
+
+            return finalStations;
+        }
+    }
+}
---
+++ GreedyAlgorithm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreedyAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var statesNeeded = new HashSet<string> { "mt", "wa", "or", "id", "nv", "ut", "ca", "az" };
+            var stations = new Dictionary<string, HashSet<string>>
+            {
+                { "kone", new HashSet<string> { "id", "nv", "ut" } },
+                { "ktwo", new HashSet<string> { "wa", "id", "mt" } },
+                { "kthree", new HashSet<string> { "or", "nv", "ca" } },
+                { "kfour", new HashSet<string> { "nv", "ut" } },
+                { "kfive", new HashSet<string> { "ca", "az" } }
+            };
+
+            var result = GreedyAlgorithm.Search(stations, statesNeeded);
+
+            Console.WriteLine(string.Join(", ", result));
+
+            Console.ReadLine();
+        }
+    }
+}
