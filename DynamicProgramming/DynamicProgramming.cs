819eb3f20a0d4cd2ca506861c55c3eef8be24773
Oleh Yashchyshy <oyashchyshyn@bizico.com>
2019-01-02 16:04:06 +0200

    add dynamic programming algorithm

---
+++ DynamicProgramming/DynamicProgramming.cs
@@ -0,0 +1,35 @@
+﻿namespace DynamicProgramming
+{
+    public static class DynamicProgramming
+    {
+        public static int[][] Find(string word, string comparedWord)
+        {
+            var mainWordLength = word.Length;
+            var comparedWordLength = comparedWord.Length;
+            int[][] matrixArray = new int[mainWordLength][];
+
+            for (int i = 0; i < mainWordLength; i++)
+            {
+                matrixArray[i] = new int[comparedWord.Length];
+
+                for (int j = 0; j < comparedWordLength; j++)
+                {
+                    if (word[i] == comparedWord[j] && (i == 0 || j == 0))
+                    {
+                        matrixArray[i][j] = 1;
+                    }
+                    else if (word[i] == comparedWord[j])
+                    {
+                        matrixArray[i][j] = matrixArray[i - 1][j - 1] + 1;
+                    }
+                    else
+                    {
+                        matrixArray[i][j] = 0;
+                    }
+                }
+            }
+
+            return matrixArray;
+        }
+    }
+}
---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DynamicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = "fish";
+            string secondWord = "finishing";
+
+            int[][] array = DynamicProgramming.Find(firstWord, secondWord);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
